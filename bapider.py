3abc36254c6ad1dd896424db002b09fbfe5a7657
Owen Chen <owenchen0630@foxmail.com>
2016-09-25 22:37:04 +0800

    success a small spider

--- bapider.py
+++ bapider.py
@@ -6,27 +6,28 @@ import thread
 import time
 
 
-pageIndex = 1
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-#初始化headers
-headers = { 'User-Agent' : user_agent }
-#存放段子的变量，每一个元素是每一页的段子们
-stories = []
-#存放程序是否继续运行的变量
-enable = False
-#传入某一页的索引获得页面代码
+def getStories():
+    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+    headers = { 'User-Agent' : user_agent }
+    print(pageIndex)
+    print('\n\n')
+    url = 'http://www.qiushibaike.com/hot/page/%s/?s=4916034' % str(pageIndex)
+    #构建请求的request
+    request = urllib2.Request(url,headers = headers)
+    #利用urlopen获取页面代码
+    response = urllib2.urlopen(request)
+    #将页面转化为UTF-8编码
+    pageCode = response.read().decode('utf-8')
+    #重点在于如何使用正则表达式进行匹配
+    pattern = re.compile('<span>.*?</span>', re.S)
+    #pattern = re.compile(u"<span>.*?[\u4e00-\u9fa5]</span>", re.S)
+    items = re.findall(pattern, pageCode)
+    for item in items:
+        if not re.search('img src', item):
+            print(item)
 
-
-url = 'http://www.qiushibaike.com/hot/' + str(pageIndex)
- #构建请求的request
-request = urllib2.Request(url,headers = headers)
-#利用urlopen获取页面代码
-response = urllib2.urlopen(request)
-#将页面转化为UTF-8编码
-pageCode = response.read().decode('utf-8')
-#重点在于如何使用正则表达式进行匹配
-pattern = re.compile('<span>.*?</span>',re.S)
-items = re.findall(pattern, pageCode)
-for item in items:
-    if not re.search('img src', item):
-        print(item)
+I = raw_input()
+for pageIndex in range(1,int(I)+1):
+    getStories()
+    raw_input()
+print("EXIT !")
